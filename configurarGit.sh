e404b019c751a3baa6ed5922e5bb6cb4e3a998ce
Josei <git@jose.ananaddy.com>
2022-09-25 01:17:47 -0300

    Agregué configuración básica de GIT

---
+++ configurarGit.sh
@@ -0,0 +1,8 @@
+# Configuracion básica de Git al instalar
+
+git config --global user.name "Josei"
+git config --global user.email "git@jose.ananaddy.com"
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
