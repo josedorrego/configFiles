b92baac3a14b454185784062a6064e451a833944
Josei <git@jose.ananaddy.com>
2022-11-27 22:07:25 -0300

    Cambios chicos, ni se que hice

--- nvim/.config/nvim/lua/packer-plugins.lua
+++ nvim/.config/nvim/lua/packer-plugins.lua
@@ -3,8 +3,21 @@ return require('packer').startup(function()
     use 'wbthomason/packer.nvim'
 
     -- colorscheme
---      use 'gruvbox-community/gruvbox'
---      use 'Shaturn/neovim-ayu'
---      use 'folke/tokyonight.nvim'
-      use "EdenEast/nightfox.nvim"
+--    use 'gruvbox-community/gruvbox'
+--    use 'Shaturn/neovim-ayu'
+--    use 'folke/tokyonight.nvim'
+    use "EdenEast/nightfox.nvim"
+    -- fin colorscheme
+    
+    -- iconos
+    use 'nvim-tree/nvim-web-devicons'
+    -- fin iconos
+
+    use 'nvim-treesitter/nvim-treesitter'
+
+    use {
+      'nvim-telescope/telescope.nvim', tag = '0.1.0',
+      -- or                            , branch = '0.1.x',
+      requires = { {'nvim-lua/plenary.nvim'} }
+    }
 end)
