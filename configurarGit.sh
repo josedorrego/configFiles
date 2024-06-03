74b54c00176be4cdd13c8cc84793fa676ef1b779
Jose Ignacio Dorrego Aroztegui <josei@JoseiM3.local>
2024-06-03 00:31:09 -0300

    Corregido mail de git y cositas de zshrcMacOS

--- configurarGit.sh
+++ configurarGit.sh
@@ -1,7 +1,7 @@
 # Configuracion básica de Git al instalar
 
 git config --global user.name "Josei"
-git config --global user.email "git@jose.ananaddy.com"
+git config --global user.email "josei@josedorrego.com"
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
